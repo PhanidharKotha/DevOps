a94a1fba555624546e4dbb893545169a771282b7
phanidevtech <phani.devtech@gmail.com>
2025-06-02 20:01:29 -0500

    sum of 2 numbers

---
+++ python/basics/2.sum.py
@@ -0,0 +1,10 @@
+#Input 2 numbers and calculate their sum without using funtions
+num1 = input("Enter your first number:")
+num2 = input("Enter your second number:")
+
+num1 = float(num1)
+num2 = float(num2)
+
+sum = num1 + num2
+
+print("Sum of 2 numbers is:", sum)
